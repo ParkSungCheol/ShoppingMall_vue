25011b765188574bc7268295ad6e026762de2c65
ParkSungCheol <skfo8gmlakd@naver.com>
2023-01-29 20:57:44 +0900

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,8 @@ export default {
   },
   methods: {
     testAxios: function() {
-      axios.get('api/data')
+      const baseURI = 'http://ec2-3-39-250-156.ap-northeast-2.compute.amazonaws.com:8080';
+      axios.get(`${baseURI}/data`)
         .then((result) => {
           console.log(result);
         })
