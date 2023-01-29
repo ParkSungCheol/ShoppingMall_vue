1bef1ab61d3b8753340736a38cbb2945546af046
ParkSungCheol <skfo8gmlakd@naver.com>
2023-01-29 13:12:14 +0900

    first

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  disableHostCheck: true
+  devServer: {
+    allowedHosts: "all",
+  },
 })
