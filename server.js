add170e4802ef6154c4f3426680654fe69b47b9f
ParkSungCheol <skfo8gmlakd@naver.com>
2023-01-29 19:11:42 +0900

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var serveStatic = require('serve-static');
 var app = express();
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 8080;
-var hostname = '127.0.0.1';
+var hostname = '0.0.0.0';
 
 app.listen(port, hostname, () => {
    console.log(`Server running at http://${hostname}:${port}/`);
