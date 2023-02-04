159b2c0124b5fe8d97fa665735d20fd7470693bf
ParkSungCheol <skfo8gmlakd@naver.com>
2023-02-04 10:09:28 +0900

    test

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,27 @@
 <template>
     <footer class="py-5 bg-dark">
         <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+        <p>test..</p>
+        <button v-on:click="testAxios">AxiosTest</button>
     </footer>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Footer',
   data () {
     return {}
   },
+  methods: {
+    testAxios: function() {
+      const baseURI = 'http://ec2-3-39-250-156.ap-northeast-2.compute.amazonaws.com:8080';
+      axios.get(`${baseURI}/data`)
+        .then((result) => {
+          console.log(result);
+        })
+    }
+  }
 }
 </script>
