4a922159681f797bd056178f9cbb392c8ebb9fee
ParkSungCheol <skfo8gmlakd@naver.com>
2023-02-10 16:17:54 +0900

    test

---
+++ src/components/good.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col mb-5">
+        <div class="card h-100">
+            <!-- Product image-->
+            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+            <!-- Product details-->
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <!-- Product name-->
+                    <h5 class="fw-bolder">Fancy Product</h5>
+                    <!-- Product price-->
+                    $40.00 - $80.00
+                </div>
+            </div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'good',
+  data () {
+    return {}
+  },
+}
+</script>
