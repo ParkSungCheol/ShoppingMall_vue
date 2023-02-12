6ef9620d9ad5f1bdd0d052cb8da1fdca5377ef02
hys <parkrad0@hanmail.net>
2023-02-12 15:53:56 +0900

    login test1

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -22,6 +22,9 @@
                       <i class="bi-cart-fill me-1"></i>
                       Cart
                       <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                      <i class="bi-cart-fill me-1"></i>
+                      login
+                      <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
                   </button>
               </form>
           </div>
