b6b524ec78e20e4f4515b35eb63f20655758d3d0
ParkSungCheol <skfo8gmlakd@naver.com>
2023-02-24 09:54:35 +0900

    2022-02-24

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="py-5 bg-dark">
-        <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023-02-24</p></div>
+        <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023-02-25</p></div>
     </footer>
 </template>
 
