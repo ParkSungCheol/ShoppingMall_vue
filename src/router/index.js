1131d54883a03498fdcddc9a4445ce50f93ee514
ParkSungCheol <skfo8gmlakd@naver.com>
2023-02-26 14:00:16 +0900

    test

--- src/router/index.js
+++ src/router/index.js
@@ -24,14 +24,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   },
-  {
-    path: '*',
-    name: 'NotFound',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: NotFoundComponent
-  }
+  // {
+  //   path: '*',
+  //   name: 'NotFound',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: NotFoundComponent
+  // }
 ]
 
 const router = createRouter({
