32167af86ea330731935336032cc036bfcd6e539
ParkSungCheol <skfo8gmlakd@naver.com>
2023-02-26 14:14:34 +0900

    test

--- src/router/index.js
+++ src/router/index.js
@@ -25,12 +25,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   },
   {
-    name: 'NotFound',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/404",
+    name: "notFound",
     component: NotFoundComponent
-  }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: "/404"
+  },
 ]
 
 const router = createRouter({
