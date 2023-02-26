6a3d2ab879a0a9816ff78df38a36f776ed153c85
ParkSungCheol <skfo8gmlakd@naver.com>
2023-02-26 13:51:49 +0900

    test

---
+++ src/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      Not Found
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  data () {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundComponent from '../components/NotFound.vue'
 
 const routes = [
   {
@@ -27,7 +28,7 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: [{ path: '/:pathMatch(.*)', component: NotFoundComponent }]
 })
 
 export default router
