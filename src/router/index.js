20c20653632273efbba83c139a08e1fb7b455b9c
ParkSungCheol <skfo8gmlakd@naver.com>
2023-02-26 13:55:54 +0900

    test

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   },
   {
-    path: '/',
+    path: '*',
     name: 'NotFound',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
