ee9ddc55867df674110cff55e052276f71ae2f1f
ParkSungCheol <skfo8gmlakd@naver.com>
2023-04-03 18:55:59 +0900

    logout test

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header></Header>
+  <LoginForm></LoginForm>
+  <Footer></Footer>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+import LoginForm from '@/components/Login.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    Header,
+    Footer,
+    LoginForm
+  }
+}
+</script>
