44d8ae8e8995131f17c8894fe92ea6c9f46a0d4d
ParkSungCheol <skfo8gmlakd@naver.com>
2023-04-04 13:25:26 +0900

    test

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -2,20 +2,94 @@
   <div>
     <div>
       <h2>Please Sign In</h2>
-      <div id="loginForm">
-        <form @submit.prevent="fnLogin">
-          <p>
-            <input class="w3-input" name="uid" placeholder="Enter your ID" v-model="user_id"><br>
-          </p>
-          <p>
-            <input name="password" class="w3-input" placeholder="Enter your password" v-model="user_pw" type="password">
-          </p>
-          <p>
-            <button type="submit" class="btn btn-outline-dark">Login</button>
-            <router-link class="btn btn-outline-dark" to="/signup">회원가입</router-link>       
-          </p>
-        </form>
-      </div>
+      <div class="member">
+        <!-- 1. 로고 -->
+        <img class="logo" src="./images/images2/logo-naver.png">
+
+        <!-- 2. 필드 -->
+        <div class="field">
+            <b>아이디</b>
+            <span class="placehold-text"><input type="text"></span>
+        </div>
+        <div class="field">
+            <b>비밀번호</b>
+            <input class="userpw" type="password">
+        </div>
+        <div class="field">
+            <b>비밀번호 재확인</b>
+            <input class="userpw-confirm" type="password">
+        </div>
+        <div class="field">
+            <b>이름</b>
+            <input type="text">
+        </div>
+
+        <!-- 3. 필드(생년월일) -->
+        <div class="field birth">
+            <b>생년월일</b>
+            <div>
+                <input type="number" placeholder="년(4자)">                
+                <select>
+                    <option value="">월</option>
+                    <option value="">1월</option>
+                    <option value="">2월</option>
+                    <option value="">3월</option>
+                    <option value="">4월</option>
+                    <option value="">5월</option>
+                    <option value="">6월</option>
+                    <option value="">7월</option>
+                    <option value="">8월</option>
+                    <option value="">9월</option>
+                    <option value="">10월</option>
+                    <option value="">11월</option>
+                    <option value="">12월</option>
+                </select>
+                <input type="number" placeholder="일">
+            </div>
+        </div>
+
+        <!-- 4. 필드(성별) -->
+        <div class="field gender">
+            <b>성별</b>
+            <div>
+                <label><input type="radio" name="gender">남자</label>
+                <label><input type="radio" name="gender">여자</label>
+                <label><input type="radio" name="gender">선택안함</label>
+            </div>
+        </div>
+
+        <!-- 5. 이메일_전화번호 -->
+        <div class="field">
+            <b>본인 확인 이메일<small>(선택)</small></b>
+            <input type="email" placeholder="선택입력">
+        </div>
+        
+        <div class="field tel-number">
+            <b>휴대전화</b>
+            <select>
+                <option value="">대한민국 +82</option>
+            </select>
+            <div>
+                <input type="tel" placeholder="전화번호 입력">
+                <input type="button" value="인증번호 받기">
+            </div>
+            <input type="number" placeholder="인증번호를 입력하세요">
+        </div>
+
+        <!-- 6. 가입하기 버튼 -->
+        <input type="submit" value="가입하기">
+
+        <!-- 7. 푸터 -->
+        <div class="member-footer">
+            <div>
+                <a href="#none">이용약관</a>
+                <a href="#none">개인정보처리방침</a>
+                <a href="#none">책임의 한계와 법적고지</a>
+                <a href="#none">회원정보 고객센터</a>
+            </div>
+            <span><a href="#none">NAVER Corp.</a></span>
+        </div>
+    </div>
     </div>
   </div>
 </template>
