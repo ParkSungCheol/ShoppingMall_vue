f64b920c28fe8d82698a50d9c9a4a11425dbefe7
ParkSungCheol <skfo8gmlakd@naver.com>
2023-04-23 16:49:24 +0900

    test

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -47,7 +47,7 @@ export default {
     test: function() {
       console.log("test");
       console.log(this.user);
-      console.log(this.$route.params.user);
+      console.log(this.$route.params);
     },
     logout: async function(event) {
       event.stopPropagation();
