b099f09f6d9f148b8d57ecbceedb280ce0cd86ab
ParkSungCheol <skfo8gmlakd@naver.com>
2023-04-23 15:28:45 +0900

    test

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -396,7 +396,7 @@ export default {
         console.log(result);
         if(result.status === 200){
           alert("회원가입이 완료되었습니다.");
-          this.$router.push('/');
+          this.$router.push('/login');
         }
         else {
           alert("회원가입에 실패하였습니다.");
