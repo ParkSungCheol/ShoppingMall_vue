91cf398712f2436aaba4ead42bd8203bea0cafb1
ParkSungCheol <skfo8gmlakd@naver.com>
2023-04-24 18:41:22 +0900

    test

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -105,6 +105,50 @@
         <!-- 6. 가입하기 버튼 -->
         <input type="submit" value="가입하기">
         </form>
+        <div id="dimmed" area-hidden="true" style="display:block;"></div>
+        <form>
+          <div class="layer" area-hidden="true" style="display:block;">
+            <div class="popup_layer_inner">
+                <div class="popup_content">
+                    <!--팝업콘텐츠영역-->
+                    <div class="contact_edit_popup" style="width:329px;">
+                        <h4 class="contact_edit_title">
+                                <strong class="bold">박성철님</strong>의 회원정보 중
+                                <em class="accent">전화번호</em>를 <span id="p_txt_phoneNo_changeYn">수정</span>하기 위해
+                                인증절차가 필요합니다.
+                        </h4>
+                        <div id="phoneNoForm" class="row_item phone" style="display: block;">
+                            <span id="p_txt_phoneNo" class="item_text">+82 10-7***-0***</span>
+                        </div>
+                        <div class="contact_form">
+                            <div class="popup_row select">
+                                <select id="internationalCode" name="internationalCode" title="전화번호" class="popup_input">
+                                  <option value="82"> 대한민국 +82</option>
+                                </select>
+                            </div>
+                            <div class="popup_row rightgap">
+                                <input type="tel" id="phoneNo" placeholder="전화번호" name="phoneNo" maxlength="14" class="popup_input" onkeydown="check_num_ajax3('phoneNo', '2', 'e_phoneNo')">
+                                <button type="button" class="btn_contact" onclick="sendSmsForChangePhoneNo()">인증</button>
+                            </div>
+                            <div class="popup_row">
+                                <input type="tel" id="authNo" placeholder="인증번호 입력" class="popup_input" onkeydown="check_num_ajax3('authNo', '2', 'e_phoneNo')" oninput="changeVerifyToPopupInput('authNo')" disabled="">
+                            </div>
+                            <p id="e_phoneNo" class="popup_error"></p>
+                        </div>
+                        <div class="btn_duo_popup">
+                            <a href="javascript:;" class="btn_item" role="button" onclick="excuteNclicksCancelAsPhoneYn();hidePhoneNumberChangePopUp()">
+                                <span class="btn_text">취소</span>
+                            </a>
+                            <a href="javascript:;" class="btn_item on" role="button" onclick="checkAuthNoForChangePhoneNo()">
+                                <span id="b_txt_phoneNo_reg" class="btn_text">변경</span>
+                            </a>
+                        </div>
+                        <button type="button" class="close_popup" onclick="excuteNclicksCloseAsPhoneYn();hidePhoneNumberChangePopUp()"><span class="blind">닫기</span></button>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </form>
     </div>
     </div>
   </div>
@@ -406,11 +450,87 @@ export default {
 </script>
 
 <style scope>
+.dimmed {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0,0,0,.5);
+    z-index: 200;
+}
+.popup_layer {
+    display: table;
+    table-layout: fixed;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    z-index: 210;
+}
+.popup_layer_inner {
+    display: table-cell;
+    vertical-align: middle;
+}
+.popup_content {
+    margin: 45px 0;
+    vertical-align: top;
+    z-index: 220;
+}
+.contact_2step_popup, .contact_edit_popup {
+    position: relative;
+    margin: 0 auto;
+    padding: 30px 24px 58px;
+    border-radius: 12px;
+    box-shadow: 0 2px 2px 0 rgba(64,117,161,.1);
+    background-color: #fff;
+    box-sizing: border-box;
+    text-align: left;
+}
+.contact_edit_title {
+    margin-bottom: 20px;
+    padding: 30px 0 14px;
+    border-bottom: 1px solid #303038;
+    font-size: 20px;
+    font-weight: 300;
+    line-height: 28px;
+    letter-spacing: -.8px;
+    color: #1e1e23;
+    word-break: break-all;
+}
+.contact_2step_popup .row_item, .contact_edit_popup .row_item {
+    padding: 0 0 20px 30px;
+}
+.contact_2step_popup .row_item+.contact_form, .contact_edit_popup .row_item+.contact_form {
+    padding-top: 0;
+}
 #member {
     box-sizing: border-box; /*전체에 박스사이징*/
     outline: none; /*focus 했을때 테두리 나오게 */
 }
-
+.btn_duo_popup {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: block;
+    text-align: center;
+}
+html[lang=ko] body, html[lang=ko] button, html[lang=ko] input, html[lang=ko] select, html[lang=ko] table {
+    font-family: -apple-system,BlinkMacSystemFont,helvetica,"Apple SD Gothic Neo",sans-serif;
+}
+.close_popup {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    vertical-align: top;
+    background-position: -164px -96px;
+    background-repeat: no-repeat;
+    width: 40px;
+    height: 40px;
+}
 body{
     font-family: 'Noto Sans KR', sans-serif;
     font-size:14px;
