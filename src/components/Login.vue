1afbad8107215493c6ed66ebdac54fe0c83388ef
ParkSungCheol <skfo8gmlakd@naver.com>
2023-04-26 17:51:42 +0900

    test

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -23,10 +23,58 @@
         </form>
       </div>
       <ul class="find_wrap">
-        <li><router-link class="find_text" to="/signup">아이디 찾기</router-link></li>
+        <li><input type="text" class="find_text" value="아이디 찾기" v-on:click="popUp('popUpPhone')"></li>
         <li><router-link class="find_text" to="/signup">비밀번호 찾기</router-link></li>
         <li><router-link class="find_text" to="/signup">회원가입</router-link></li>
       </ul>
+      <div class="dimmed" area-hidden="true" style="display:none;" ref="dimmed"></div>
+        <form>
+          <div class="layer" area-hidden="true" style="display:none;" ref="layer">
+            <div class="popup_layer">
+            <div class="popup_layer_inner">
+                <div class="popup_content">
+                    <!--팝업콘텐츠영역-->
+                    <div class="contact_edit_popup" style="width:329px;">
+                        <h4 class="contact_edit_title">
+                                <strong class="bold">{{user.name}}님</strong>의 회원정보 중<br/>
+                                <em class="accent">{{ popUpPwd? '패스워드' : popUpEmail? '이메일주소' : '전화번호' }}</em>를 <span id="p_txt_phoneNo_changeYn">수정</span>하기 위해<br/>
+                                인증절차가 필요합니다.
+                        </h4>
+                        <div v-show="popUpPhone">
+                        <div id="phoneNoForm" class="row_item phone" style="display: block;">
+                            <span id="p_txt_phoneNo" class="item_text">[ 기존 전화번호 ] <br/>{{ user.phone }}</span>
+                        </div>
+                        <div class="contact_form">
+                            <div class="popup_row select">
+                                <select id="internationalCode" name="internationalCode" title="전화번호" class="popup_input">
+                                  <option value="82"> 대한민국 +82</option>
+                                </select>
+                            </div>
+                            <div class="popup_row rightgap">
+                                <input type="tel" placeholder="변경할 전화번호 입력" v-on:keyup="keyPress($event, 'phone')" ref="sendMessage" class="popup_input" :disabled="!sendMessage">
+                                <button type="button" class="btn_contact" v-on:click="messageCheck('sendMessage')" :disabled="!sendMessage">인증</button>
+                                <b style="color:red" v-show="phone">입력한 전화번호를 확인하세요</b>                          
+                            </div>
+                            <div class="popup_row">
+                                <input type="text" placeholder="인증번호를 입력하세요" ref="checkMessage" maxlength="30" class="popup_input" :disabled="!checkMessage">
+                            </div>
+                            <p id="e_phoneNo" class="popup_error"></p>
+                        </div>
+                        </div>
+                        <div class="btn_duo_popup">
+                            <a href="javascript:;" class="btn_item" role="button" v-on:click="closePopUp">
+                                <span class="btn_text">취소</span>
+                            </a>
+                            <a href="javascript:;" class="btn_item on" role="button" v-on:click="changePopUp">
+                                <span id="b_txt_phoneNo_reg" class="btn_text">변경</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+          </div>
+        </form>
     </div>
   </div>
 </template>
@@ -43,9 +91,29 @@ export default {
       loginSuccess:false,
       error:false,
       selected: false,
+      phone: true,
+      sendMessage : true,
+      checkMessage : false,
     }
   },
   methods: {
+    closePopUp() {
+      this.$refs.dimmed.style.display = 'none';
+      this.$refs.layer.style.display = 'none';
+      document.querySelector("*").style.overflow = 'visible';
+      this.$refs.sendMessage.value = "";
+      this.$refs.checkMessage.value = "";
+      this.phone = true;
+      this.sendMessage = true;
+      this.checkMessage = false;
+    },
+    popUp(target) {
+      this[target] = true;
+      this.$refs.dimmed.style.display = 'block';
+      this.$refs.layer.style.display = 'block';
+      document.querySelector("*").style.overflow = 'hidden';
+      //window.scrollTo(0,0);
+    },
     async fnLogin() {
       if(!this.user_id) { alert("아이디를 입력해주세요!"); return; }
       if(!this.user_pw) { alert("패스워드를 입력해주세요!"); return; }
