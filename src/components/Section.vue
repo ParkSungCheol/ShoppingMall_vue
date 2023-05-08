92f2e6dcadcc11b96e58df6c20c321122d85ab42
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-08 15:49:04 +0900

    test

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -58,7 +58,10 @@
               </svg>
             </a>
             <div class="pagination_num">
-              <a class="pagination_btn_page" v-for="i in filteredNum" href="javascript:void(0);" v-on:click="movePage(i, 'current')" v-bind:style="{ color: !searchDto && i == 1? 'red' : searchDto && searchDto.page == i ? 'red' : 'black' }">{{ i }}</a>
+              <a class="pagination_btn_page" v-for="i in filteredNum" href="javascript:void(0);" v-on:click="movePage(i, 'current')" v-bind:style="{ 
+                color: !searchDto && i == 1? 'red' : searchDto && searchDto.page == i ? 'red' : 'black',
+                pointer: !searchDto && i == 1? none : searchDto && searchDto.page == i ? none : 'black'
+                }">{{ i }}</a>
             </div>
             <a class="pagination_next" v-if="pagination && pagination.existNextPage" href="javascript:void(0);" v-on:click="movePage(pagination.endPage + 1, 'forward')">
               다음
