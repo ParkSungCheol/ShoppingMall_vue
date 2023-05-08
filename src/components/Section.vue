efa3cac07a247121977229f6dd59a969a081ae23
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-08 13:19:10 +0900

    test

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -43,13 +43,13 @@
                     </div>
                 </div>
             </ul>
-            <a v-if="pagination.existPrevPage" href="javascript:void(0);" onclick="movePage(1)" class="page_bt first">첫 페이지</a>
-            <a v-if="pagination.existPrevPage" href="javascript:void(0);" onclick="movePage(${pagination.startPage - 1})" class="page_bt prev">이전 페이지</a>
+            <a v-if="pagination && pagination.existPrevPage" href="javascript:void(0);" onclick="movePage(1)" class="page_bt first">첫 페이지</a>
+            <a v-if="pagination && pagination.existPrevPage" href="javascript:void(0);" onclick="movePage(${pagination.startPage - 1})" class="page_bt prev">이전 페이지</a>
             <p>
-              <a v-for="i in pagination.endPage - pagination.startPage + 1" v-if="i >= pagination.startPage && i <= pagination.endPage" href="javascript:void(0);" onclick="movePage(${i});">{{ i }}</a>
+              <a v-for="i in pagination.endPage - pagination.startPage + 1" v-if="pagination && i >= pagination.startPage && i <= pagination.endPage" href="javascript:void(0);" onclick="movePage(${i});">{{ i }}</a>
             </p>
-            <a v-if="pagination.existNextPage" href="javascript:void(0);" onclick="movePage(${pagination.endPage + 1});" class="page_bt next">다음 페이지</a>
-            <a v-if="pagination.existNextPage" href="javascript:void(0);" onclick="movePage(${pagination.totalPageCount});" class="page_bt last">마지막 페이지</a>
+            <a v-if="pagination && pagination.existNextPage" href="javascript:void(0);" onclick="movePage(${pagination.endPage + 1});" class="page_bt next">다음 페이지</a>
+            <a v-if="pagination && pagination.existNextPage" href="javascript:void(0);" onclick="movePage(${pagination.totalPageCount});" class="page_bt last">마지막 페이지</a>
           </div>
       </div>
   </section>
