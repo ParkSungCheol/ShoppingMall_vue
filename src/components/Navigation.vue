62936e230ddf2a1ac4e44b57dc237bd4cdbe49cb
ParkSungCheol <skfo8gmlakd@naver.com>
2023-05-11 15:34:11 +0900

    test

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container px-4 px-lg-5">
+      <div class="container px-lg-5">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
@@ -82,8 +82,6 @@ export default {
 <style scoped>
 nav {
   padding: 5px 0px !important;
-  padding-left: 0px !important;
-  padding-right: 0px !important;
 }
 .right_menu {
   width: 82%;
