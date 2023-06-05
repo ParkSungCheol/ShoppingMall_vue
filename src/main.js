c5105d18739e3e3b82a41775140e092d9e69271b
ParkSungCheol <skfo8gmlakd@naver.com>
2023-06-05 10:05:55 +0900

    test

--- src/main.js
+++ src/main.js
@@ -3,5 +3,4 @@ import App from './App.vue'
 import router from './router'
 import { LoadingPlugin } from 'vue-loading-overlay'
 
-Vue.use(LoadingPlugin)
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(LoadingPlugin).mount('#app')
