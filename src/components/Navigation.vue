183562bc474a13b4dfd35a715b7bc68de9dd88f0
ParkSungCheol <skfo8gmlakd@naver.com>
2023-07-03 10:29:07 +0900

    test

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,8 +4,16 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                  <li class="nav-item"><router-link class="nav-link active" aria-current="page" to="/">Home</router-link></li>
-                  <li class="nav-item"><router-link class="nav-link" to="/data">Data</router-link></li>
+                  <li class="nav-item"><router-link class="nav-link" to="/" v-bind:style="{ 
+                  color: link == 'Home'? 'red' : 'black',
+                  cursor: link == 'Home'? 'default' : 'pointer',
+                  'font-weight': link == 'Home'? 'bold' : 'normal',
+                  }" @click="change('Home')">Home</router-link></li>
+                  <li class="nav-item"><router-link class="nav-link" to="/data" v-bind:style="{ 
+                  color: link == 'Data'? 'red' : 'black',
+                  cursor: link == 'Data'? 'default' : 'pointer',
+                  'font-weight': link == 'Data'? 'bold' : 'normal',
+                  }" @click="change('Data')">Data</router-link></li>
               </ul>
           </div>
           <div class="right_menu">
@@ -31,6 +39,7 @@ export default {
     return {
       user : null,
       cartCount : 0,
+      link : 'Home'
     }
   },
   props : {
@@ -41,6 +50,9 @@ export default {
     this.getSearch();
   },
   methods: {
+    change(msg) {
+      this.link = msg;
+    },
     async getSearch() {
       try {
         const baseURI = 'https://api.jurospring.o-r.kr';
