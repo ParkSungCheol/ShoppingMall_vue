34c86dc14dfeb245ac6cd46c8c53f0b674f8f6ac
ParkSungCheol <skfo8gmlakd@naver.com>
2023-07-04 11:21:53 +0900

    test

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,7 @@
     <header class="bg-dark py-5">
         <div class="container px-4 px-lg-5 my-5">
             <div class="text-center text-white">
-                <h1 class="display-4 fw-bolder">JuroMarket</h1>
-                <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                <h1 class="display-4 fw-bolder">JuroSpring Market</h1>
             </div>
         </div>
     </header>
