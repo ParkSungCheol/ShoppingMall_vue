d7cf9c033174be1a3f76bee5b04d6a8fbc279abd
ParkSungCheol <skfo8gmlakd@naver.com>
2023-07-08 18:35:39 +0900

    footer css 변경

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,6 @@
 <template>
-<<<<<<< HEAD
-    <footer class="py-5 bg-dark footer">
-        <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023-03-03</p></div>
-=======
     <footer class="py-5 bg-dark">
         <div class="container"><p class="m-0 text-center text-white">Copyright &copy; JuroSpring Market</p></div>
->>>>>>> 4238d2a2262bdc382a463cbbe6682adea095258a
     </footer>
 </template>
 
